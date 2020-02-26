c28ee10c1847803992bfa84c78d75759c64a73c5
Srisanmathi <sanmathi3896@gmail.com>
2020-02-26 16:46:16 -0500

    included setInterval

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
+const axios = require('axios')
 
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+
+    setInterval(()=>{
+        res.send("hello");
+    },10000);
+    
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
